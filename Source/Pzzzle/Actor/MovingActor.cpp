58e348d25d0dfe41601d8fce9ae6564c5cdf3737
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2022-11-04 07:31:47 -0700

    running code on sever side then doing replication on cclient side

--- Source/Pzzzle/Actor/MovingActor.cpp
+++ Source/Pzzzle/Actor/MovingActor.cpp
@@ -5,21 +5,30 @@
 
 AMovingActor::AMovingActor()
 {
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = true;//enable tick
 	this->Speed = 10.5f;
 	SetMobility(EComponentMobility::Movable);
 };
 void AMovingActor::BeginPlay()
 {
 	Super::BeginPlay();
-	if (HasAuthority())UE_LOG(LogTemp,Warning, TEXT("Running on Server..."))
-	else UE_LOG(LogTemp,Warning, TEXT("Running on Client..."));
+	if (HasAuthority())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[C++]Code Running on Server Side..."));
+		SetReplicates(true);	//enable replication to the client side from server side
+		SetReplicateMovement(true);	//enable movement replication to the client side from server side
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[C++]Code Running on Client Side..."));
+	};
+
 
 };
 void AMovingActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	if (HasAuthority())
+	if (HasAuthority())	//code will on run server side then it will replicate to client side if SetReplicates(true) and SetReplicateMovement(true)
 	{
 		/*auto*/FVector Location = GetActorLocation();
 		Location += FVector(Speed * DeltaTime, 0, 0);
