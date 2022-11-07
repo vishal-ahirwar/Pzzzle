d61078dbf808a03b738f76eb540c3e9618ec3dfb
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2022-11-07 12:32:13 +0530

    OnComponentBeginOverlap.AddDynami

---
+++ Source/Pzzzle/Actor/PlatformTrigger.h
@@ -0,0 +1,26 @@
+// Copyright(c)2022 Vishal Ahirwar. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlatformTrigger.generated.h"
+
+UCLASS()
+class PZZZLE_API APlatformTrigger : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APlatformTrigger();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
