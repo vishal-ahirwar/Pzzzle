e2773578479c05c637c6279919bedadcd6f4abed
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2022-11-27 21:09:05 +0530

    unreal interface added

---
+++ Source/Pzzzle/MenuSystem/MenuInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright(c)2022 Vishal Ahirwar. All rights reserved.
+
+
+#include "MenuInterface.h"
+
+// Add default functionality here for any IMenuInterface functions that are not pure virtual.
---
+++ Source/Pzzzle/MenuSystem/MenuInterface.h
@@ -0,0 +1,25 @@
+// Copyright(c)2022 Vishal Ahirwar. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "MenuInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UMenuInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class PZZZLE_API IMenuInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
