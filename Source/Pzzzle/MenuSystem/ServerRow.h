59f83ac64affb1f88c4708495e300e0e1bed35ee
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2022-12-06 15:33:58 +0530

    ServerRow.cpp added

---
+++ Source/Pzzzle/MenuSystem/ServerRow.h
@@ -0,0 +1,17 @@
+// Copyright(c)2022 Vishal Ahirwar. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ServerRow.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PZZZLE_API UServerRow : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
